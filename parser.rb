775d219ac44330090cdc412c675e63441c6b3f4b
Olga <ovin999@gmail.com>
2022-09-18 12:50:57 +0300

    remove instance variables

--- parser.rb
+++ parser.rb
@@ -3,30 +3,30 @@ require 'debug'
 class CimpParser
   def self.parse(file = 'files/TEST_FBL_0702.txt')
     if File.exist?(file)
-      @arr_lines = File.open(file, 'r').to_a.map!(&:chomp)
-      @flight_params = @arr_lines[1].split("/")
+      arr_lines = File.open(file, 'r').to_a.map!(&:chomp)
+      flight_params = arr_lines[1].split("/")
 
-      @airbills = []
-      @arr_lines.map { |line| @airbills << line if line.split("-").size == 2 }
+      airbills = []
+      arr_lines.map { |line| airbills << line if line.split("-").size == 2 }
 
-      print_report
+      print_report(arr_lines, flight_params, airbills)
     else
       puts 'Sorry, the file was not found'
     end
   end
 
-  def self.print_report
+  def self.print_report(arr_lines, flight_params, airbills)
     puts 'Flight params:'
-    puts "ID Flight - #{@flight_params[0]}"
-    puts "Flight No - #{@flight_params[1]}"
-    puts "Date Flight - #{@flight_params[2]}"
-    puts "Departure - #{@flight_params[3]}"
-    puts "Destination - #{@arr_lines[2]}"
-    puts "Aircraft Registration - #{@flight_params[4]}"
+    puts "ID Flight - #{flight_params[0]}"
+    puts "Flight No - #{flight_params[1]}"
+    puts "Date Flight - #{flight_params[2]}"
+    puts "Departure - #{flight_params[3]}"
+    puts "Destination - #{arr_lines[2]}"
+    puts "Aircraft Registration - #{flight_params[4]}"
     puts
     puts
 
-    @airbills.each do |bill|
+    airbills.each do |bill|
       puts "airbill number - " + bill.split("/")[0]
       puts "weight - " + bill.split("/")[1].split(".").last
       puts "seat number - " + bill.split("/")[1].split(".")[1]
